82bbb21cba876ec14666c94c34197a79f7b65667
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 01:31:33 -0300

    Update kick message for manual rejoin trigger

--- Rejoin on Kick or Afk.lua
+++ Rejoin on Kick or Afk.lua
@@ -2,4 +2,4 @@ game:GetService("GuiService").ErrorMessageChanged:Connect(function()
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end)
 
---game.Players.LocalPlayer:Kick() -- uncomment to trigger rejoin manually / just rejoin.
+--game.Players.LocalPlayer:Kick("\nRejoining...") -- uncomment to trigger rejoin manually / just rejoin.
