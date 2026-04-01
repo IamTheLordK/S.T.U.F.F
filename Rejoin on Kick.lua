195be1770a3df3bc9c34454d6428a1e568e009c8
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 20:55:38 -0300

    Delete Rejoin on Kick.lua

--- Rejoin on Kick.lua
+++
@@ -1,5 +0,0 @@
-game:GetService("GuiService").ErrorMessageChanged:Connect(function() -- fires when any error message appears kick, inactivity, etc.
-game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
-end)
-
---game.Players.LocalPlayer:Kick("\nRejoining...") -- uncomment to trigger rejoin manually / just rejoin.
