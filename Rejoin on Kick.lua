2c8e77c4cae6eab2dab5a44f6bddef5980a75fe2
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 02:25:36 -0300

    Handle rejoin on kick or inactivity
    Connect to ErrorMessageChanged to handle rejoining on kick or inactivity.

--- Rejoin on Kick.lua
+++ Rejoin on Kick.lua
@@ -1,4 +1,4 @@
-game:GetService("GuiService").ErrorMessageChanged:Connect(function()
+game:GetService("GuiService").ErrorMessageChanged:Connect(function() -- fires when any error message appears kick, inactivity, etc.
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end)
 
