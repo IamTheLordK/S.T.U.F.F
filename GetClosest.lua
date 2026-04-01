b013029e70b7fbf3305b805f403ca9b0401a69ba
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 19:26:33 -0300

    Rename variables for clarity in GetClosest.lua

--- GetClosest.lua
+++ GetClosest.lua
@@ -1,10 +1,10 @@
 local Char = game.Players.LocalPlayer.Character
 
-local Player, Dist = nil, math.huge
+local Target, Distance = nil, math.huge
 for _, v in pairs(game.Players:GetPlayers()) do
  if v == game.Players.LocalPlayer then continue end
  local D = v:DistanceFromCharacter(Char.HumanoidRootPart.Position)
- if D > 0 and D < Dist then Player, Dist = v, D end
+ if D > 0 and D < Distance then Target, Distance = v, D end
 end
 
 print(Player and Player.Name or "None")
