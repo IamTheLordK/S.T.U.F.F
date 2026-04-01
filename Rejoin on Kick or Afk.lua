602ce1af0abc95cfd4f4dc1d6a5e23cf6b5d986a
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 01:26:58 -0300

    Simplify rejoin logic on kick or AFK

--- Rejoin on Kick or Afk.lua
+++ Rejoin on Kick or Afk.lua
@@ -1,10 +1,5 @@
 game:GetService("GuiService").ErrorMessageChanged:Connect(function()
-if #game.Players:GetPlayers() <= 1 then
- game.Players.LocalPlayer:Kick("\nRejoining...")
- wait()
- game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-else
- game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
-end
+game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end)
+
 --game.Players.LocalPlayer:Kick() -- uncomment to trigger rejoin manually / just rejoin.
