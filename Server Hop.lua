1350c87200894b0810edb7fd9fcda137c8f2ffb1
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 01:16:20 -0300

    Enhance server hop functionality with notifications
    Updated server hopping logic to include notifications and handle empty server lists.

--- Server Hop.lua
+++ Server Hop.lua
@@ -1,7 +1,18 @@
+game.StarterGui:SetCore("SendNotification", {Title = "[ Server hop ]", Text = "Searching for a server...", Duration = 5})
+
+local function Hop()
 local S = {}
 
-for _, v in pairs(game:GetService("HttpService"):JSONDecode(request({Url="https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true"}).Body).data) do
+for _, v in pairs(game:GetService("HttpService"):JSONDecode(request({Url = "https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true"}).Body).data or {}) do
  if v.id ~= game.JobId then table.insert(S, v.id) end
 end
 
-if #S > 0 then game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, S[math.random(1, #S)]) end
+if #S > 0 then
+ game.StarterGui:SetCore("SendNotification", {Title = "[ Server hop ]", Text = "Server found!", Duration = 5})
+ game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, S[math.random(#S)])
+ else wait() Hop()
+ end
+end
+
+game:GetService("GuiService").ErrorMessageChanged:Connect(Hop)
+Hop()
