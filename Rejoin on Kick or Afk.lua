6ffc5f25ca4473f5c36fd2a4ce16f57c6614a7e0
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-04-01 00:27:26 -0300

    Fix wait function for rejoining on kick or AFK

---
+++ Rejoin on Kick or Afk.lua
@@ -0,0 +1,9 @@
+game:GetService("GuiService").ErrorMessageChanged:Connect(function()
+if #game.Players:GetPlayers() <= 1 then
+ game.Players.LocalPlayer:Kick("\nRejoining...")
+ wait()
+ game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+else
+ game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+end
+end)
