c0152576434fb8ea575581033895a5285ef1f82a
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-06-03 03:33:12 -0300

    Update character reference and movement logic

--- [ FUN ] Mimicry.lua
+++ [ FUN ] Mimicry.lua
@@ -1,9 +1,11 @@
-local TChar = game.Players["Here..."].Character
+local TChar = game.Players["srheading"].Character
 local Root = game.Players.LocalPlayer.Character.HumanoidRootPart
+local TRoot = TChar.HumanoidRootPart
 
-setscriptable(Root.Parent.Humanoid, "WalkDirection", true)
+Root.Parent.Humanoid.AutoRotate = false
+Root.CFrame = TRoot.CFrame
 
-while Root.Parent and TChar.HumanoidRootPart do game:GetService("RunService").RenderStepped:Wait()
- Root.Parent.Humanoid.WalkDirection = TChar.Humanoid.MoveDirection
- Root.CFrame = CFrame.Angles(0, math.atan2(-TChar.HumanoidRootPart.CFrame.LookVector.X, -TChar.HumanoidRootPart.CFrame.LookVector.Z), 0) + Root.Position
+while Root.Parent and TRoot do game:GetService("RunService").RenderStepped:Wait()
+ Root.Parent.Humanoid:Move(TChar.Humanoid.MoveDirection, false)
+ Root.CFrame = CFrame.Angles(0, math.atan2(-TRoot.CFrame.LookVector.X, -TRoot.CFrame.LookVector.Z), 0) + Root.Position
 end
