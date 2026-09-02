bc0f9cf187e4269c0c9b1ada5ae545080203a5eb
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-09-02 11:07:13 -0300

    Refactor nitro toggle functionality and GUI creation

--- [ FUN ] Vehicle-Boost.lua
+++ [ FUN ] Vehicle-Boost.lua
@@ -1,11 +1,9 @@
 --allows using a continuous nitro when pressing E, E to toggle.
 local Speed = 250
 
-local S = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
-local Box = Instance.new("TextBox", S)
+local Box = Instance.new("TextBox", Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui))
 
-S.ResetOnSpawn = false
-
+Box.Parent.ResetOnSpawn = false
 Box.Size = UDim2.new(0, 100, 0, 26)
 Box.Position = UDim2.new(0, 8, 0, 8)
 Box.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
@@ -16,17 +14,19 @@ Box.Text = tostring(Speed)
 Box.PlaceholderText = "Speed"
 
 Box.FocusLost:Connect(function()
- Speed = tonumber(Box.Text) or Speed
+Speed = tonumber(Box.Text) or Speed
 end)
 
-game:GetService("UserInputService").InputBegan:Connect(function(Key)
- if Key.KeyCode ~= Enum.KeyCode.E then return end
- local Seat = game.Players.LocalPlayer.Character.Humanoid.SeatPart
- if not Seat then return end
- if Seat:FindFirstChild("BodyVelocity") then Seat.BodyVelocity:Destroy() return end
+game:GetService("UserInputService").InputBegan:Connect(function(i)
+if i.KeyCode ~= Enum.KeyCode.E then return end
+local Char = game.Players.LocalPlayer.Character
+local Humanoid = Char and Char:FindFirstChild("Humanoid")
+local Seat = Humanoid and Humanoid.SeatPart
+if not Char or not Humanoid or not Seat then return end
+if Seat:FindFirstChild("BodyVelocity") then Seat.BodyVelocity:Destroy() return end
 
- local BodyV = Instance.new("BodyVelocity", Seat)
- BodyV.MaxForce = Vector3.new(9e9, 0, 9e9)
+local BodyV = Instance.new("BodyVelocity", Seat)
+BodyV.MaxForce = Vector3.new(9e9, 0, 9e9)
  while BodyV.Parent do game:GetService("RunService").RenderStepped:Wait()
   BodyV.Velocity = Seat.CFrame.LookVector * Speed
  end
