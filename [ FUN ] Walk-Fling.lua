9eef2be7a595f9243d234d8edc23fd19ff0f88fb
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-09-12 16:21:26 -0300

    Update Walk-Fling.lua to improve character movement

--- [ FUN ] Walk-Fling.lua
+++ [ FUN ] Walk-Fling.lua
@@ -1,11 +1,9 @@
 local Char = game:GetService("Players").LocalPlayer.Character
 Char.Humanoid:ChangeState(11)
 
-while Char.Parent do game:GetService("RunService").Heartbeat:Wait()
+while Char:IsDescendantOf(workspace) and game:GetService("RunService").Heartbeat:Wait() do
  local V = Char.HumanoidRootPart.AssemblyLinearVelocity
- Char.HumanoidRootPart.AssemblyLinearVelocity = V * 2^63
+ Char.HumanoidRootPart.AssemblyLinearVelocity = Vector3.one * 9e9
  game:GetService("RunService").RenderStepped:Wait()
- Char.HumanoidRootPart.AssemblyLinearVelocity = V
- game:GetService("RunService").Stepped:Wait()
  Char.HumanoidRootPart.AssemblyLinearVelocity = V
 end
