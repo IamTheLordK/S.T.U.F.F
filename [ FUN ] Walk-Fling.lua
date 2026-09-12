3be4dcf86a6f3e9ac6618a34284d3dbdc0593630
IamTheLordK <234321964+IamTheLordK@users.noreply.github.com>
2026-09-12 16:22:09 -0300

    Increase AssemblyLinearVelocity value in Walk-Fling.lua

--- [ FUN ] Walk-Fling.lua
+++ [ FUN ] Walk-Fling.lua
@@ -3,7 +3,7 @@ Char.Humanoid:ChangeState(11)
 
 while Char:IsDescendantOf(workspace) and game:GetService("RunService").Heartbeat:Wait() do
  local V = Char.HumanoidRootPart.AssemblyLinearVelocity
- Char.HumanoidRootPart.AssemblyLinearVelocity = Vector3.one * 9e9
+ Char.HumanoidRootPart.AssemblyLinearVelocity = Vector3.one * 2^63
  game:GetService("RunService").RenderStepped:Wait()
  Char.HumanoidRootPart.AssemblyLinearVelocity = V
 end
